2d48496c21e446722759027be7ce4e9172e45070
Aidar Shaikhutdin @makeweb.space <makeweb.space@gmail.com>
2021-05-04 15:00:29 +0300

    Add: base html markup

--- src/init.js
+++ src/init.js
@@ -1,7 +1,3 @@
-import Example from './Example.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default () => {
-  const element = document.getElementById('point');
-  const obj = new Example(element);
-  obj.init();
-};
+export default () => {};
